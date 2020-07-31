0b5bb7407036a182f2a9f7665ebb3720ca6d12f5
Santiago Dávalos <santiagodavalos@Santiagos-MacBook-Air.local>
2020-07-31 15:55:18 -0400

    Solving String Repeat

---
+++ string-repeat.js
@@ -0,0 +1,11 @@
+function repeatStr(n, s) {
+  return s.repeat(n);
+}
+
+
+// expect to return ***
+console.log(repeatStr(3, "*"));
+// expect to return #####
+console.log(repeatStr(5, "#"));
+// expect to return ha ha 
+console.log(repeatStr(2, "ha "));
